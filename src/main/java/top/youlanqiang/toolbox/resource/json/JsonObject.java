f3b5f7d6676d368afd67e9e02a8a1d49e34fd487
youlanqiang <youlanqiang@hotmail.com>
2023-02-20 22:48:21 +0800

    feat: 新建Json操作类

---
+++ src/main/java/top/youlanqiang/toolbox/resource/json/JsonObject.java
@@ -0,0 +1,93 @@
+package top.youlanqiang.toolbox.resource.json;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * JSON对象
+ * 
+ * @author youlanqiang
+ */
+public class JsonObject implements Map<String, Object> {
+
+    private Map<String, Object> map;
+
+    public JsonObject() {
+        this.map = new HashMap<>();
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Object get(Object key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object put(String key, Object value) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object remove(Object key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends String, ? extends Object> m) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Set<String> keySet() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Collection<Object> values() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<Entry<String, Object>> entrySet() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
